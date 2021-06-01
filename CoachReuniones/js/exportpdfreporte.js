2ad7d8cc7230980db592a41f421a056dcfdabff6
carlossalazar7 <carlossalazar2228@gmail.com>
2021-06-01 17:50:28 -0600

    pdf export

--- CoachReuniones/js/exportpdfreporte.js
+++ CoachReuniones/js/exportpdfreporte.js
@@ -1,6 +1,6 @@
 window.jsPDF = window.jspdf.jsPDF;
 
-const ExportarPDF = (datos) =>{
+const ExportarPDF = (datos) => {
     const btnexportPdf = document.querySelectorAll((".btnexpPdf"));
     let universidad = [];
 
@@ -17,92 +17,49 @@ const ExportarPDF = (datos) =>{
 
         //construyendo la información
         let data = {
-            columns:[{
+            columns: [{
                 No: "No.",
                 nombre: "Alumno",
                 universidad: "Universidad",
-                tipo: "Tipo de materias",
+                class: "Class",
+                status: "Status",
                 promedio: "Promedio",
-                materias: "Materias"
+                materias1: "No. mat. Aprobadas",
+                materias2: "No. mat. Reprobadas",
+                materias3: "No. mat. Retiradas"
             }],
-            rows:[]
+            rows: []
         };
         let alumnotmp = "";
         let contador = 1;
         //obteniendo datos de aprobados
-        const listAprobados = universidad.listaAprobado;
+        const listAprobados = universidad.l1;
+        const listRetirados = universidad.listaRetirado;
+        const listReprobados = universidad.listaReprobado;
+
         for (const key in listAprobados) {
             if (alumnotmp !== listAprobados[key].alumno) {
                 alumnotmp = listAprobados[key].alumno;
+
                 data.rows.push({
                     No: contador++,
                     nombre: listAprobados[key].alumno,
                     universidad: universidad.id,
-                    tipo: "Aprobadas",
-                    promedio: PromedioMaterias(listAprobados.filter(x => x.alumno === alumnotmp)),
-                    materias: NombreMaterias(listAprobados.filter(x => x.alumno === alumnotmp)),
+                    class: listAprobados[key].Class,
+                    status: listAprobados[key].estatus,
+                    promedio: listAprobados[key].promedio,
+                    materias1: listAprobados[key].total,
+                    materias2: (listReprobados.filter(x => x.alumno === alumnotmp).length),
+                    materias3: (listRetirados.filter(x => x.alumno === alumnotmp).length),
                 });
+
             }
         }
-
-        //obteniendo datos de retirados
-        alumnotmp = "";
-        const listRetirados = universidad.listaRetirado;
-        for (const key in listRetirados) {
-            if (alumnotmp !== listRetirados[key].alumno) {
-                alumnotmp = listRetirados[key].alumno;
-                data.rows.push({
-                    No: contador++,
-                    nombre: listRetirados[key].alumno,
-                    universidad: universidad.id,
-                    tipo: "Retiradas",
-                    promedio: PromedioMaterias(listRetirados.filter(x => x.alumno === alumnotmp)),
-                    materias: NombreMaterias(listRetirados.filter(x => x.alumno === alumnotmp)),
-                });
-            }
-        }
-
-        //obteniendo datos de reprobados
-        alumnotmp = "";
-        const listReprobados = universidad.listaReprobado;
-        for (const key in listReprobados) {
-            if (alumnotmp !== listReprobados[key].alumno) {
-                alumnotmp = listReprobados[key].alumno;
-                data.rows.push({
-                    No: contador++,
-                    nombre: listReprobados[key].alumno,
-                    universidad: universidad.id,
-                    tipo: "Reprobadas",
-                    promedio: PromedioMaterias(listReprobados.filter(x => x.alumno === alumnotmp)),
-                    materias: NombreMaterias(listReprobados.filter(x => x.alumno === alumnotmp)),
-                });
-            }
-        }
-
         console.log(data);
         MakePDf(data)
     }));
 
-    function PromedioMaterias(materias) {
-        let promedio = 0;
-        materias.forEach(materia => {
-            promedio += parseFloat(materia.nota);
-        });
-        promedio = promedio/materias.length;
-        return parseFloat(promedio).toFixed(2);
-    }
-
-    function NombreMaterias(materias) {
-        let nombres = "";
-        materias.forEach(materia => {
-            nombres += materia.nombreMateria + ", ";   
-        });
-
-        return nombres;
-    }
-
-    function MakePDf(data) 
-    {
+    function MakePDf(data) {
         var pdfdoc = new jsPDF("1", "pt");
         pdfdoc.setFontSize(12);
         pdfdoc.setTextColor(0);
