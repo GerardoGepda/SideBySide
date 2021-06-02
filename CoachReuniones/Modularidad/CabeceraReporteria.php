2f3ec469dda48b23e35fd6937346035eaf642c7e
carlossalazar7 <carlossalazar2228@gmail.com>
2021-06-02 16:34:15 -0600

    exporting style

--- CoachReuniones/Modularidad/CabeceraReporteria.php
+++ CoachReuniones/Modularidad/CabeceraReporteria.php
@@ -56,6 +56,32 @@
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <!--Estilo nuevo de reporteriaUniversidad-->
 <style>
+    #rotate {
+        -webkit-animation: spin 4s linear infinite;
+        -moz-animation: spin 4s linear infinite;
+        animation: spin 4s linear infinite;
+    }
+
+    @-moz-keyframes spin {
+        100% {
+            -moz-transform: rotate(360deg);
+        }
+    }
+
+    @-webkit-keyframes spin {
+        100% {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+
+    @keyframes spin {
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
+    }
+    
+
     .form-label {
         color: #be0032;
         font-weight: bold;
--- CoachReuniones/js/exportpdfreporte.js
+++ CoachReuniones/js/exportpdfreporte.js
@@ -5,59 +5,92 @@ const ExportarPDF = (datos) => {
     let universidad = [];
 
     btnexportPdf.forEach(btn => btn.addEventListener("click", (e) => {
-        //extraemos el id de la universidad del btn
-        const idU = e.target.classList[0].replace("-", " ");
-        //filtramos los datos para la U seleccionada
-        for (const U in datos) {
-            if (datos[U].id === idU) {
-                universidad = datos[U];
-                break;
-            }
-        }
+        let icon = `<i class="fas fa-spinner " id="rotate" ></i>`;
+        btn.innerHTML = icon;
+        setTimeout(
+            () => {
+                //extraemos el id de la universidad del btn
+                const idU = e.target.classList[0].replace("-", " ");
+                //filtramos los datos para la U seleccionada
+                for (const U in datos) {
+                    if (datos[U].id === idU) {
+                        universidad = datos[U];
+                        break;
+                    }
+                }
 
-        //construyendo la información
-        let data = {
-            columns: [{
-                No: "No.",
-                nombre: "Alumno",
-                universidad: "Universidad",
-                class: "Class",
-                status: "Status",
-                promedio: "Promedio",
-                materias1: "No. mat. Aprobadas",
-                materias2: "No. mat. Reprobadas",
-                materias3: "No. mat. Retiradas"
-            }],
-            rows: []
-        };
-        let alumnotmp = "";
-        let contador = 1;
-        //obteniendo datos de aprobados
-        const listAprobados = universidad.l1;
-        const listRetirados = universidad.listaRetirado;
-        const listReprobados = universidad.listaReprobado;
+                //construyendo la información
+                let data = {
+                    columns: [{
+                        No: "No.",
+                        nombre: "Alumno",
+                        universidad: "Universidad",
+                        class: "Class",
+                        status: "Status",
+                        promedio: "Promedio",
+                        materias1: "No. mat. Aprobadas",
+                        materias2: "No. mat. Reprobadas",
+                        materias3: "No. mat. Retiradas"
+                    }],
+                    rows: []
+                };
+                let alumnotmp = "";
+                let contador = 1;
+                //obteniendo datos de aprobados
+                const listAprobados = universidad.l1;
+                const lis2 = universidad.listaAprobado;
+                const listRetirados = universidad.listaRetirado;
+                const listReprobados = universidad.listaReprobado;
 
-        for (const key in listAprobados) {
-            if (alumnotmp !== listAprobados[key].alumno) {
-                alumnotmp = listAprobados[key].alumno;
+                for (const key in listAprobados) {
+                    if (alumnotmp !== listAprobados[key].alumno) {
+                        alumnotmp = listAprobados[key].alumno;
+                        let promedi = 0;
+                        let reprob = parseFloat(PromedioMaterias(listReprobados.filter(x => x.alumno === alumnotmp)))
+                        let aprob = parseFloat(PromedioMaterias(lis2.filter(x => x.alumno === alumnotmp)))
 
-                data.rows.push({
-                    No: contador++,
-                    nombre: listAprobados[key].alumno,
-                    universidad: universidad.id,
-                    class: listAprobados[key].Class,
-                    status: listAprobados[key].estatus,
-                    promedio: listAprobados[key].promedio,
-                    materias1: listAprobados[key].total,
-                    materias2: (listReprobados.filter(x => x.alumno === alumnotmp).length),
-                    materias3: (listRetirados.filter(x => x.alumno === alumnotmp).length),
-                });
+                        if (isNaN(reprob)) {
+                            promedi = aprob
+                        } else {
+                            promedi = ((aprob + reprob + 1) / 2).toFixed(2)
+                        }
 
-            }
-        }
-        console.log(data);
-        MakePDf(data)
+                        data.rows.push({
+                            No: contador++,
+                            nombre: listAprobados[key].alumno,
+                            universidad: universidad.id,
+                            class: listAprobados[key].Class,
+                            status: listAprobados[key].estatus,
+                            promedio: promedi,
+                            materias1: listAprobados[key].total,
+                            materias2: (listReprobados.filter(x => x.alumno === alumnotmp).length),
+                            materias3: (listRetirados.filter(x => x.alumno === alumnotmp).length),
+                        });
+
+                    }
+                }
+                MakePDf(data)
+                btn.innerHTML = `<i class="fas fa-file-pdf"></i>`;
+            }, 5000, "Finished"
+        );
+
     }));
+
+
+    function PromedioMaterias(materias) {
+        let promedio = 0;
+        materias.forEach(materia => {
+            promedio += parseFloat(materia.nota);
+        });
+        promedio = promedio / materias.length;
+        if (isNaN(promedio)) {
+            return null
+        } else {
+            return parseFloat(promedio).toFixed(2);
+        }
+    }
+
+
 
     function MakePDf(data) {
         var pdfdoc = new jsPDF("1", "pt");
